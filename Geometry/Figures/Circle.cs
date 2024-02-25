2045c50a63b1e403754775f4673d409d0210bf09
cherry-pdb <mark.v.nazarov@yandex.ru>
2024-02-25 21:17:28 +0300

    add task

---
+++ Geometry/Figures/Circle.cs
@@ -0,0 +1,19 @@
+﻿namespace Geometry.Figures;
+
+public class Circle : IShape
+{
+    private double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        if (radius < 0)
+            throw new ArgumentException("Radius must be non-negative");
+        
+        Radius = radius;
+    }
+    
+    public double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
---
+++ Geometry/Figures/Triangle.cs
@@ -0,0 +1,39 @@
+﻿namespace Geometry.Figures;
+
+public class Triangle : IShape
+{
+    private double FirstSide { get; set; }
+    private double SecondSide { get; set; }
+    private double ThirdSide { get; set; }
+
+    public Triangle(double firstSide, double secondSide, double thirdSide)
+    {
+        if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0)
+            throw new ArgumentException("Sides must be positive.");
+        
+        FirstSide = firstSide;
+        SecondSide = secondSide;
+        ThirdSide = thirdSide;
+    }
+    
+    public double CalculateArea()
+    {
+        var halfPerimeter = (FirstSide + SecondSide + ThirdSide) / 2;
+       
+        return Math.Sqrt(halfPerimeter 
+                         * (halfPerimeter - FirstSide) 
+                         * (halfPerimeter - SecondSide) 
+                         * (halfPerimeter - ThirdSide));
+    }
+
+    public bool IsRightAngled()
+    {
+        var firstPow = FirstSide * FirstSide;
+        var secondPow = SecondSide * SecondSide;
+        var thirdPow = ThirdSide * ThirdSide;
+
+        return firstPow + secondPow == thirdPow 
+            || firstPow + thirdPow == secondPow
+            || secondPow + thirdPow == firstPow;
+    }
+}
